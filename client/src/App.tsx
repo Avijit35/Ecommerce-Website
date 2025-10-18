b5cca3826637d4744c898c08468978701d75d85c
Avijit Manna <avijit.manna@isubsoft.com>
2025-10-19 02:41:44 +0530

    initial setup and Create header

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import Footer from "./ui/Footer";
+import Header from "./ui/Header";
+import Container from "./ui/Container";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Container>
+        <p>Ecommerce demo</p>
+      </Container>
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
