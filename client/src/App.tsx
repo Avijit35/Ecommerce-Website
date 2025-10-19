fe5699f698d4336f4d2facf58874ff737a3d86ab
Avijit Manna <avijit.manna@isubsoft.com>
2025-10-19 11:06:54 +0530

    Router setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,11 @@
-import Footer from "./ui/Footer";
-import Header from "./ui/Header";
 import Container from "./ui/Container";
 
 function App() {
   return (
     <main>
-      <Header />
       <Container>
         <p>Ecommerce demo</p>
       </Container>
-      <Footer />
     </main>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,83 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Layout from "./ui/Layout.tsx";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Product from "./pages/Product.tsx";
+import Category from "./pages/Category.tsx";
+import Profile from "./pages/Profile.tsx";
+import Favourite from "./pages/Favourite.tsx";
+import Cart from "./pages/Cart.tsx";
+import Cancel from "./pages/Cancel.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import Orders from "./pages/Orders.tsx";
+import Success from "./pages/Success.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const RouterLayout = () => {
+  return (
+    <Layout>
+      <Outlet />
+    </Layout>
+  );
+};
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RouterLayout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/product",
+        element: <Product />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+      {
+        path: "/category",
+        element: <Category />,
+      },
+      {
+        path: "/category/:id",
+        element: <Category />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/favourite",
+        element: <Favourite />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/cancel",
+        element: <Cancel />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
+      },
+      {
+        path: "/success",
+        element: <Success />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={Router} />
+);
