f13390d874344234007e5986551ca9fe74623e0e
Avijit Manna <avijit.manna@isubsoft.com>
2025-10-20 11:03:06 +0530

    Dynamic routing in backend

--- admin/index.mjs
+++ admin/index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import path from "path";
+import { readdirSync } from "fs";
 // import { fileURLToPath } from "url";
 
 const app = express();
@@ -13,6 +14,13 @@ app.use(express.json());
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 const __dirname = path.resolve();
+const routesPath = path.resolve(__dirname, "./routes");
+const routeDir = readdirSync(routesPath);
+
+routeDir.map(async (file) => {
+  const routeModule = await import(`./routes/${file}`);
+  app.use("/", routeModule.default);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
